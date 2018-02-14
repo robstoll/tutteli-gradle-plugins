76bfad838e7c9ea161c8b44a870083c3fd3142e1
Robert Stoll <rstoll@tutteli.ch>
2018-02-15 00:52:24 +0100

    add plugin functions used in settings.gradle
    - introduce SettingsUtilPlugin.groovy
      - document it in README -> switch to asciidoc for fun ;)
    - add a gradle task repo which puts the resulting jar into a local repo
      - setup the repo in settings.gradle

---
+++ tutteli-gradle-settings/src/main/groovy/ch/tutteli/gradle/settings/SettingsUtilPlugin.groovy
@@ -0,0 +1,27 @@
+package ch.tutteli.gradle.settings
+
+import org.gradle.api.Plugin
+import org.gradle.api.initialization.Settings
+
+class SettingsUtilPlugin implements Plugin<Settings> {
+    @Override
+    void apply(Settings settings) {
+
+        def includeCustomInFolder = {String folder, String customName ->
+            settings.include ":$customName"
+            settings.project(":$customName").projectDir = new File(
+                "${settings.rootProject.projectDir}/$folder/$customName"
+            )
+        }
+
+        settings.ext.includeCustomInFolder = includeCustomInFolder
+
+        settings.ext.includeInFolder = { String folder, String nameWithoutPrefix ->
+            includeCustomInFolder(folder, "${settings.rootProject.name}-$nameWithoutPrefix")
+        }
+
+        settings.ext.includeOwn = { String nameWithoutPrefix ->
+            includeCustomInFolder("${settings.rootProject.name}-$nameWithoutPrefix", "")
+        }
+    }
+}
