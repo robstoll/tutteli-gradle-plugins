21836bab602f66f7595e685b2f7504470a3b6565
Robert Stoll <rstoll@tutteli.ch>
2018-02-21 23:27:02 +0100

    write smoke test for jacoco plugin
    - split up the apply method into applyJunit and applyJacoco
      - do not use closure to configure objects but use `with` (so that we
        gain a bit of type-safety)

--- tutteli-gradle-jacoco/src/main/groovy/ch/tutteli/gradle/jacoco/JacocoPlugin.groovy
+++ tutteli-gradle-jacoco/src/main/groovy/ch/tutteli/gradle/jacoco/JacocoPlugin.groovy
@@ -2,45 +2,54 @@ package ch.tutteli.gradle.jacoco
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.testing.jacoco.plugins.JacocoPluginExtension
 import org.gradle.testing.jacoco.tasks.JacocoReport
+import org.junit.platform.gradle.plugin.JUnitPlatformPlugin
 
 class JacocoPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
+        Task junitPlatformTestTask = applyJunitPlatform(project)
+        applyJacoco(project, junitPlatformTestTask)
+    }
 
-        //TODO check if we can modify buildscript classpath but I doubt it
-//        project.buildscript.dependencies {
-//            classpath "org.junit.platform:junit-platform-gradle-plugin:1.0.3"
-//        }
-        project.getPluginManager().apply('jacoco')
-        project.getPluginManager().apply('org.junit.platform.gradle.plugin')
+    private static Task applyJunitPlatform(Project project) {
+        project.getPluginManager().apply(JUnitPlatformPlugin)
 
         def junitPlatformTestTask = project.tasks.getByName('junitPlatformTest')
         //TODO make configurable, with/without junit report
         def reportIndex = junitPlatformTestTask.args.findIndexOf { it == '--reports-dir' }
         def keep = (0..(junitPlatformTestTask.args.size() - 1)) - [reportIndex, reportIndex + 1]
         junitPlatformTestTask.args = junitPlatformTestTask.args[keep]
+        return junitPlatformTestTask
+    }
 
-        project.jacoco {
+    private static void applyJacoco(Project project, Task junitPlatformTestTask) {
+        project.getPluginManager().apply(org.gradle.testing.jacoco.plugins.JacocoPlugin)
+
+        JacocoPluginExtension jacocoExtension = project.extensions.getByName('jacoco') as JacocoPluginExtension
+        jacocoExtension.with {
             toolVersion = '0.8.0'
             applyTo junitPlatformTestTask
         }
 
         //TODO make configurable currently one would need to do tasks.getByName('junitPlatformJacocoReport')
-        def jacocoReport = project.task(type: JacocoReport, 'junitPlatformJacocoReport',
-            {
-                sourceDirectories = project.sourceSets.main.allJava.sourceDirectories
-                classDirectories = project.files(project.sourceSets.main.output.classesDirs)
-                executionData junitPlatformTestTask
-                reports {
-                    csv.enabled = false
-                    csv.destination project.file("${project.jacoco.reportsDir}/junitReport.csv")
-                    xml.enabled = false
-                    xml.destination project.file("${project.jacoco.reportsDir}/junitReport.xml")
-                    html.enabled = false
-                    html.destination project.file("${project.jacoco.reportsDir}/junitHtml/")
-                }
-            })
+        JacocoReport jacocoReport = project.task(type: JacocoReport, 'junitPlatformJacocoReport') as JacocoReport
+        jacocoReport.with {
+            sourceDirectories = project.sourceSets.main.allJava.sourceDirectories
+            classDirectories = project.files(project.sourceSets.main.output.classesDirs)
+            executionData junitPlatformTestTask
+            reports {
+                csv.enabled = false
+                csv.destination project.file("${project.jacoco.reportsDir}/junitReport.csv")
+                xml.enabled = false
+                xml.destination project.file("${project.jacoco.reportsDir}/junitReport.xml")
+                html.enabled = false
+                html.destination project.file("${project.jacoco.reportsDir}/junitHtml/")
+            }
+        }
         project.check.dependsOn jacocoReport
     }
+
 }
