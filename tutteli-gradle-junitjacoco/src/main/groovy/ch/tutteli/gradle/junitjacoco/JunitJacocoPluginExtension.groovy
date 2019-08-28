a77d261921d1297d326c4f765ca13ba82366cd18
Robert Stoll <rstoll@tutteli.ch>
2019-08-28 10:30:41 +0200

    set a default configuration for testLogging

--- tutteli-gradle-junitjacoco/src/main/groovy/ch/tutteli/gradle/junitjacoco/JunitJacocoPluginExtension.groovy
+++ tutteli-gradle-junitjacoco/src/main/groovy/ch/tutteli/gradle/junitjacoco/JunitJacocoPluginExtension.groovy
@@ -3,8 +3,11 @@ package ch.tutteli.gradle.junitjacoco
 import org.gradle.api.Action
 import org.gradle.api.Project
 import org.gradle.api.provider.Property
+import org.gradle.api.tasks.testing.Test
 import org.gradle.testing.jacoco.plugins.JacocoPluginExtension
 import org.gradle.testing.jacoco.tasks.JacocoReport
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 class JunitJacocoPluginExtension {
     private JacocoPluginExtension jacocoPluginExtension
@@ -22,11 +25,10 @@ class JunitJacocoPluginExtension {
         this.enableJunitReport = project.objects.property(Boolean)
         this.enableJunitReport.set(false)
         project.check.dependsOn jacocoReportTask
-        defaultConfig()
+        defaultConfig(project)
     }
 
-    private void defaultConfig() {
-
+    private void defaultConfig(Project project) {
         jacoco {
             toolVersion = '0.8.3'
         }
@@ -42,16 +44,24 @@ class JunitJacocoPluginExtension {
                 html.destination project.file("${project.jacoco.reportsDir}/html/")
             }
         }
+
+        project.tasks.withType(Test) {
+            testLogging {
+                events  TestLogEvent.FAILED,
+                        TestLogEvent.SKIPPED
+                        TestLogEvent.STANDARD_OUT
+                exceptionFormat TestExceptionFormat.FULL
+                showExceptions true
+                showCauses true
+                showStackTraces true
+            }
+        }
     }
 
     void jacoco(Action<JacocoPluginExtension> configure) {
         configure.execute(jacocoPluginExtension)
     }
 
-    /**
-     * @deprecated Use the gradle API instead: jacocoTestReport {...}
-     */
-    @Deprecated(/*since = "0.28.0"*/)
     void jacocoReport(Action<JacocoReport> configure) {
         configure.execute(jacocoReportTask)
     }
