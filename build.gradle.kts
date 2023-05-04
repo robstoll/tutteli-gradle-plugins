b4ff09ea1553d9a8f48007a1aed8e6c7f3fe4e55
Robert Stoll <rstoll@tutteli.ch>
2023-05-04 07:05:01 +0200

    v4.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,8 @@ val mavenModelVersion by extra("3.8.7")
 val jacocoToolVersion by extra("0.8.8")
 
 buildscript {
-    val version = "4.7.2"
-    val previousVersion = "4.7.1"
+    val version = "4.8.0"
+    val previousVersion = "4.7.2"
 
     rootProject.version = version
     rootProject.group = "ch.tutteli"
