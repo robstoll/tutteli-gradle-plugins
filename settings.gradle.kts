2e311f8a05c5a6690e0016aee012825fc2f34f26
Robert Stoll <robert.stoll@tegonal.com>
2023-10-23 21:56:12 +0200

    remove kotlin-utils and project-utils modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,8 +4,6 @@ listOf(
     "dokka",
     "junitjacoco",
     "kotlin-module-info",
-    "kotlin-utils",
-    "project-utils",
     "publish",
     "spek"
 ).forEach { include("${rootProject.name}-$it") }
