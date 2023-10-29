8b5bb4e64e2b7ac03f16e68cfcecf5d2c1e57f4e
Robert Stoll <robert.stoll@tegonal.com>
2023-10-29 21:28:02 +0100

    spek cleanup, drop support for old multiplatform plugins

--- tutteli-gradle-spek/src/test/groovy/ch/tutteli/gradle/plugins/spek/SpekPluginIntTest.groovy
+++ tutteli-gradle-spek/src/test/groovy/ch/tutteli/gradle/plugins/spek/SpekPluginIntTest.groovy
@@ -18,37 +18,15 @@ import static org.junit.jupiter.api.Assumptions.assumeFalse
 @ExtendWith(SettingsExtension)
 class SpekPluginIntTest {
 
-
-    @Test
-    void smokeTest_OldJvmPlatform(SettingsExtensionObject settingsSetup) throws IOException {
-        checkSmokeTest(settingsSetup, "kotlin-platform-jvm", '1.6.21')
-    }
-
-
     @ParameterizedTest
     @MethodSource("kotlinVersionAndGradle")
     void smokeTest_JvmPlatform(String kotlinVersion, String gradleVersion, SettingsExtensionObject settingsSetup) throws IOException {
-        checkSmokeTest(settingsSetup, "org.jetbrains.kotlin.jvm", kotlinVersion, gradleVersion)
-    }
-
-    static List<Arguments> kotlinVersionAndGradle() {
-        def javaVersion = JavaVersion.toVersion(System.getProperty("java.version"))
-        return ['1.3.61', '1.4.10', '1.5.21', '1.6.20', '1.7.20', '1.8.10'].collectMany { kotlinVersion ->
-            def gradleVersions = ((javaVersion < JavaVersion.VERSION_15) ? ['6.9.4', '7.6.1'] : ['7.6.1']) + (kotlinVersion.matches("^1\\.[7-9].*\$") ? ['8.0'] : [])
-            gradleVersions.collect { gradleVersion ->
-                Arguments.of(kotlinVersion, gradleVersion)
-            }
-        }
-    }
-
-    private static void checkSmokeTest(SettingsExtensionObject settingsSetup, String kotlinPlugin, String kotlinVersion, String gradleVersion = null) {
-        //arrange
         settingsSetup.settings << """
         rootProject.name='test-project'
         """
         settingsSetup.buildGradle << """
         ${settingsSetup.buildscriptWithKotlin(kotlinVersion)}
-        apply plugin: '$kotlinPlugin'
+        apply plugin: 'org.jetbrains.kotlin.jvm'
         apply plugin: 'ch.tutteli.gradle.plugins.spek'
         spek.version = '2.0.15'
 
@@ -72,7 +50,6 @@ class SpekPluginIntTest {
             }
         })
         """
-        //act
         def builder = GradleRunner.create()
         if (gradleVersion != null) {
             builder = builder.withGradleVersion(gradleVersion)
@@ -81,9 +58,18 @@ class SpekPluginIntTest {
             .withProjectDir(settingsSetup.tmp)
             .withArguments("clean", "build")
             .build()
-        //assert
         assertTrue(result.output.contains("was here"), "println in output:\n" + result.output)
         def failed = result.taskPaths(TaskOutcome.FAILED)
         assertTrue(failed.empty, 'FAILED is empty but was not: ' + failed)
     }
+
+    static List<Arguments> kotlinVersionAndGradle() {
+        return ['1.7.20', '1.8.10', '1.9.10'].collectMany { kotlinVersion ->
+            def gradleVersions = ['8.1.1', '8.3']
+            gradleVersions.collect { gradleVersion ->
+                Arguments.of(kotlinVersion, gradleVersion)
+            }
+        }
+    }
+
 }
