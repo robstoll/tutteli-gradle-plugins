6a73a6fb73d1b745dd9b454a01c8304b1699f440
Robert Stoll <robert.stoll@tegonal.com>
2023-10-29 21:07:18 +0100

    move createClasspathManifest logic into build-logic
    and filter out gradle dependencies

--- gradle/build-logic/dev/src/main/kotlin/build-logic.gradle-plugin.gradle.kts
+++ gradle/build-logic/dev/src/main/kotlin/build-logic.gradle-plugin.gradle.kts
@@ -9,19 +9,33 @@ gradlePlugin {
     website.set("https://github.com/robstoll/tutteli-gradle-plugins")
     vcsUrl.set("https://github.com/robstoll/tutteli-gradle-plugins.git")
 }
-//
-//val createClasspathManifest = tasks.register("createClasspathManifest") {
-//    val outputDir = file("${buildDir}/${name}")
-//
-//    inputs.files(sourceSets.main.get().runtimeClasspath)
-//    outputs.dir(outputDir)
-//
-//    doLast {
-//        outputDir.mkdirs()
-//        file("${outputDir}/plugin-classpath.txt").writeText(sourceSets.main.get().runtimeClasspath.joinToString("\n"))
-//    }
-//}
-//tasks.named("test") {
-//    dependsOn(createClasspathManifest)
-//}
 
+val createClasspathManifest = tasks.register("createClasspathManifest") {
+    val outputDir = file("${buildDir}/${name}")
+
+    inputs.files(sourceSets.main.get().runtimeClasspath)
+    outputs.dir(outputDir)
+
+    doLast {
+        outputDir.mkdirs()
+        file("${outputDir}/plugin-classpath.txt").writeText(
+            sourceSets.main.get().runtimeClasspath
+                .filterNot {
+                    it.path.contains("dists${File.separator}/gradle-")
+                }
+                .joinToString("\n"))
+    }
+}
+tasks.named("test") {
+    dependsOn(createClasspathManifest)
+}
+
+dependencies {
+    testRuntimeOnly(files(createClasspathManifest)) //required for tests
+
+    if (rootProject.name != "gradle-kotlin-dsl-accessors") {
+        testImplementation(project(":test-utils")) {
+            exclude(group = "org.codehaus.groovy")
+        }
+    }
+}
